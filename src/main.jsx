0117add9fa70ca6c3339878b2178435203bd6b3d
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-08 20:26:12 +0200

    add reload page button

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 import StoreFront from './StoreFront';
 import './index.css';
 
@@ -6,4 +7,23 @@ function App() {
   return <StoreFront />;
 }
 
-createRoot(document.getElementById('root')).render(<App />);
+// add reload page button
+let root = createRoot(document.getElementById('root'));
+
+const reload = () => {
+  root.unmount();
+  root = createRoot(document.getElementById('root'));
+  console.log('Page reloaded');
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+};
+reload();
+
+const button = document.createElement('button');
+button.textContent = 'Reload page';
+button.classList.add('btn-reload');
+button.addEventListener('click', reload);
+document.body.insertAdjacentElement('afterbegin', button);
